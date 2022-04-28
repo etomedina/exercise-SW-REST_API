2bdab9c59524bb1cafc8321e1632ff09a17b2afa
etomedina <hmedina@LAPTOP-84M4TVEB.localdomain>
2022-04-28 19:24:09 -0400

    Primer commit

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
+import os, requests
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Person, Planet, Vehicle
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,123 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
+
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
+
+#     return jsonify(response_body), 200
+
+# @app.route('/people2', methods=['GET'])
+# def get_people():
+#     requests.get('https://www.swapi.tech/api/')
+#     return 
+
+#poblar base de dato de personajes
+base_url = "https://www.swapi.tech/api/"
+@app.route('/poblar-personaje', methods=['GET'])
+def get_poblar_personaje():
+    response =  requests.get(f"{base_url}{'people'}/?page=1")
+    results = response.json()['results']
+    count = 0 
+    for xdict in results:
+        response = requests.get(xdict['url'])
+        detaresponse = response.json()['result']['properties']
+        person = Person.created(**detaresponse) 
+        if person != None:
+            count = count  + 1
+    return jsonify({'count':count}),200
+
+#poblar base de dato de planetas
+@app.route('/poblar-planeta', methods=['GET'])
+def get_poblar_planeta():
+    response =  requests.get(f"{base_url}{'planets'}/?page=1")
+    results = response.json()['results']
+    count = 0 
+    for xdict in results:
+        response = requests.get(xdict['url'])
+        detaresponse = response.json()['result']['properties']
+        planet = Planet.created(**detaresponse) 
+        if planet != None:
+            count = count  + 1
+    return jsonify({'count':count}),200
+
+#poblar base de datos de vehiculos
+@app.route('/poblar-vehiculo', methods=['GET'])
+def get_poblar_vehiculo():
+    response =  requests.get(f"{base_url}{'vehicles'}/?page=1")
+    results = response.json()['results']
+    count = 0 
+    for xdict in results:
+        response = requests.get(xdict['url'])
+        detaresponse = response.json()['result']['properties']
+        vehicle = Vehicle.created(**detaresponse) 
+        if vehicle != None:
+            count = count  + 1
+    return jsonify({'count':count}),200
+
+
+#1. obtener people
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    people= Person.query.all()
+    list_people= list(map(
+        lambda item: item.serialize(), people))
+    return jsonify(list_people),200
+
+
+#2. obtener people/especifica
+@app.route('/people/<int:id>', methods=['GET'])
+def more_details_person(id):
+    #buscar en base de datos al pesonaje cuya id corresponde a la suya
+    person = Person.query.get(id)
+    if isinstance(person,Person):
+        #enviar vista detallada del personaje 
+        dictionary = person.serialize(long=True)
+        return jsonify(dictionary),200
+    return 404
+
+#3. obtener planetas
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets= Planet.query.all()
+    list_planets= list(map(
+        lambda item: item.serialize(), planets))
+    return jsonify(list_planets),200
+
+#4. obtener planetas/especificos
+@app.route('/planet/<int:id>', methods=['GET'])
+def more_details_planet(id):
+    #buscar en base de datos del planeta cuya id corresponde a la suya
+    planet = Planet.query.get(id)
+    if isinstance(planet,Planet):
+        #enviar vista detallada del planeta
+        dictionary = planet.serialize(long=True)
+        return jsonify(dictionary),200
+    return 404
+
+#5. obtener usuarios
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_user():
+    users= User.query.all()
+    list_users= list(map(
+        lambda item: item.serialize(), users))
+    return jsonify(list_users),200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#6. obtener usuarios/favoritos
 
-    return jsonify(response_body), 200
+#7. postear favorito, planeta por su id
+
+#8. postear favorito, people por su id
+
+
+#9. borrar favorito, planeta por su id
+
+#10. borrar favorito, people por su id
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,7 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    description = db.Column(db.String(500))
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +18,167 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Person(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    height = db.Column(db.String(80)) 
+    mass = db.Column(db.String(80)) 
+    hair_color = db.Column(db.String(80)) 
+    skin_color = db.Column(db.String(80)) 
+    eye_color = db.Column(db.String(80)) 
+    birth_year = db.Column(db.String(80)) 
+    gender = db.Column(db.String(80)) 
+    created = db.Column(db.String(80)) 
+    edited = db.Column(db.String(80)) 
+    name = db.Column(db.String(80)) 
+    homeworld = db.Column(db.String(80)) 
+    url = db.Column(db.String(80)) 
+
+    @classmethod 
+    def created(cls,**data):
+        persona = cls(**data)
+        if (not isinstance(persona,cls)): 
+            print("Fallo en la instancia")
+            return None
+        db.session.add(persona)
+        try:
+            db.session.commit()
+            return persona
+        except Exception as error:
+            return None
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender" : self.gender,
+            "created" : self.created,
+            "edited" : self.edited,
+            "name"    : self.name,
+            "homeworld" : self.homeworld,
+            "url"   : self.url
+        }
+
+class Planet(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    diameter = db.Column(db.String(80)) 
+    rotation_period = db.Column(db.String(80)) 
+    orbital_period = db.Column(db.String(80)) 
+    gravity = db.Column(db.String(80)) 
+    population = db.Column(db.String(80)) 
+    climate = db.Column(db.String(80)) 
+    terrain = db.Column(db.String(80)) 
+    surface_water = db.Column(db.String(80)) 
+    created = db.Column(db.String(80))
+    edited = db.Column(db.String(80)) 
+    name = db.Column(db.String(80)) 
+    url = db.Column(db.String(80)) 
+
+    @classmethod 
+    def created(cls,**data):
+        planeta = cls(**data)
+        if (not isinstance(planeta,cls)): 
+            print("Fallo en la instancia")
+            return None
+        db.session.add(planeta)
+        try:
+            db.session.commit()
+            return planeta
+        except Exception as error:
+            return None
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "gravity": self.gravity,
+            "population": self.population,
+            "climate": self.climate,
+            "terrain" : self.terrain,
+            "surface_water" : self.surface_water,
+            "created" : self.created,
+            "edited" : self.edited,
+            "name"    : self.name,
+            "homeworld" : self.homeworld,
+            "url"   : self.url
+        }
+
+class Vehicle(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(80)) 
+    vehicle_class = db.Column(db.String(80)) 
+    manufacturer = db.Column(db.String(80)) 
+    cost_in_credits = db.Column(db.String(80)) 
+    length = db.Column(db.String(80)) 
+    crew = db.Column(db.String(80)) 
+    passengers = db.Column(db.String(80)) 
+    max_atmosphering_speed = db.Column(db.String(80))
+    cargo_capacity = db.Column(db.String(80))
+    consumables = db.Column(db.String(80))
+    films = db.Column(db.String(80))
+    pilots = db.Column(db.String(80))     
+    created = db.Column(db.String(80))
+    edited = db.Column(db.String(80)) 
+    name = db.Column(db.String(80)) 
+    url = db.Column(db.String(80)) 
+
+    @classmethod 
+    def created(cls,**data):
+        vehiculo = cls(**data)
+        if (not isinstance(vehiculo,cls)): 
+            print("Fallo en la instancia")
+            return None
+        db.session.add(vehiculo)
+        try:
+            db.session.commit()
+            return vehiculo
+        except Exception as error:
+            return None
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "model": self.model,
+            "vehicle_class": self.vehicle_class,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "crew": self.crew,
+            "passengers" : self.passengers,
+            "max_atmosphering_speed" : self.max_atmosphering_speed,
+            "cargo_capacity" : self.cargo_capacity,
+            "consumables" : self.consumables,
+            "films" : self.films,
+            "pilots" : self.pilots,
+            "created" : self.created,
+            "edited" : self.edited,
+            "name"    : self.name,
+            "homeworld" : self.homeworld,
+            "url"   : self.url
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    vehicle_id= db.Column(db.Integer, db.ForeignKey('vehicle.id'), primary_key=True)
+    planet_id= db.Column(db.Integer, db.ForeignKey('planet.id'), primary_key=True)
+    person_id= db.Column(db.Integer, db.ForeignKey('person.id'), primary_key=True)
+
+    def __repr__(self):
+        return f"< user_id: {self.user_id}, Card_id: {self.card_id}>"
+   
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "card_id": self.card_id
+        }       
